d49165ebbbf6ba68683b55a8091abe6a5dd91e92
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-07 16:52:22 +0530

    Added the basic structure of the application
    Added changes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+// Declare express server and dotenv for config properties
 const express = require('express')
-// Used to manage env variables
-//Just like application.prop in springboot
-const dotenv = require('dotenv')
+const dotenv = require('dotenv')// Used to manage env variables.Just like application.prop in springboot
+const morgan = require('morgan')// For logging
+const exphbs= require('express-handlebars') //as a template engine
+const connectDB = require('./config/db')
 
 // Load config:set the path to config file
 dotenv.config({path: './config/config.env'})
+connectDB()
+
+// Initialize app with express
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// HandleBars
+app.engine('.hbs', exphbs({ defaultLayout:'main',extname: '.hbs'}));
+app.set('view engine','.hbs');
+
+
+const PORT = process.env.port || 5000
+// call application
+app.listen(PORT, 
+    console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
+
+
+

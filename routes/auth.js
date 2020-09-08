1c69bf657c18b3e72db55407630e59a2bde61968
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-08 19:44:08 +0530

    Completed Authentication and redirecting to Dashboard.

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,17 @@ const router = express.Router()
 
 // @desc    Auth with Google
 // @route   GET /
-router.get('/google', passport.authenticate('google',{ scope: [ 'profile' ] }))
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 //@desc Google Auth callback
 //@route GET/auth/google/callback
-router.get('/auth/google/callback', passport.authenticate('google',{ failiureRedirect: 
-    '/' }), (req, res) => {
-        res.redirect('/dashboard')
-    } )
+router.get(
+    '/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/dashboard')
+    }
+  )
 
 
 module.exports = router
